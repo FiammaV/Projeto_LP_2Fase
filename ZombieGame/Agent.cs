838b3e95372c557fcf56fff2bdecb1c18f7938e3
FiammaV <ines.perdigao.nunes@gmail.com>
2018-06-25 17:05:46 +0100

    Added properties

--- ZombieGame/Agent.cs
+++ ZombieGame/Agent.cs
@@ -5,7 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ZombieGame {
-    class Agent {
-
+    public class Agent : IGameObject {
+        public int Column { get; set; }
+        public int Row { get; set; }
+        public AgentType Type { get; set; }
+        public bool Playable { get; set; }
     }
 }
