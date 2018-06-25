57bcd5f7f0ec5362415dc97798da14b480bdb243
FiammaV <ines.perdigao.nunes@gmail.com>
2018-06-25 18:31:45 +0100

    Added and started World.cs

---
+++ ZombieGame/World.cs
@@ -0,0 +1,11 @@
+﻿namespace ZombieGame {
+    public class World {
+
+        private IGameObject[] agents;
+        private IGameObject[,] grid;
+
+        public World(Config c) {
+            agents = new IGameObject[c.InitialHumans + c.InitialZombies];
+        }
+    }
+}
