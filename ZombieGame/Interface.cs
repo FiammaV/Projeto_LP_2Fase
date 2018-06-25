24f1b4eeb5dfce82c88f60a1a6a54807796c39f7
ineesgoncalvees <ines98goncalves@gmail.com>
2018-06-25 15:58:58 +0100

    Method ShowWorld

--- ZombieGame/Interface.cs
+++ ZombieGame/Interface.cs
@@ -4,7 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ZombieGame {
-    class Interface {
+namespace ZombieGame
+{
+    class Interface
+    {
+        public void ShowWorld(IGameObject[,] grid)
+        {
+            char state = ' ';
+            for (int i = 0; i < grid.GetLength(0); i++)
+            {
+                for (int j = 0; j < grid.GetLength(1); j++)
+                {
+                    state = State(grid[i, j]);
+                    Console.Write(state);
+                    Console.Write("\t");
+                }
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("");
+            }
+        }
+
+        public char State(IGameObject go)
+        {
+            char state = ' ';
+
+            if (go is Empty)
+            {
+                state = '.';
+            }
+
+            return state;
+        }
     }
 }
+
