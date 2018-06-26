fd55a66266c7d4c5fcc87ed5141e9aaf4b706fb9
FiammaV <ines.perdigao.nunes@gmail.com>
2018-06-26 09:22:30 +0100

    Continued World

--- ZombieGame/World.cs
+++ ZombieGame/World.cs
@@ -3,9 +3,22 @@
 
         private IGameObject[] agents;
         private IGameObject[,] grid;
+        public int Row { get; set; }
+        public int Column { get; set; }
 
         public World(Config c) {
             agents = new IGameObject[c.InitialHumans + c.InitialZombies];
+
+            grid = new IGameObject[Row, Column];
+
+            for (int i = 0; i < grid.GetLength(0); i++) {
+                for (int j = 0; j < grid.GetLength(1); j++) {
+                    grid[i, j] = new Empty();
+                }
+            }
+
+            UserInterface it = new UserInterface();
+            it.ShowWorld(grid);
         }
     }
 }
