f5b0c408055e7cc6edd65a824cf3fadc5bee17ca
sera pinta <serapinta90@hotmail.com>
2018-06-26 15:49:14 +0100

    Adicionado comentário

--- ZombieGame/Agent.cs
+++ ZombieGame/Agent.cs
@@ -5,13 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ZombieGame {
+    /// <summary>
+    /// Agent implement IGameObject
+    /// </summary>
     public class Agent : IGameObject {
-       
+       // Properties
         public int Column { get; }
         public int Row { get; }
         public AgentType Type { get; set; }
         public bool Playable { get; set; }
 
+        // Empty constructor 
         public Agent(AgentType agent)
         {
         }
