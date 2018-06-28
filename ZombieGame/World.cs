6fb3be8e662ff15d6f262d311f88dcbb76844f9f
FiammaV <ines.perdigao.nunes@gmail.com>
2018-06-28 11:20:00 +0100

    Fixed bugs

--- ZombieGame/World.cs
+++ ZombieGame/World.cs
@@ -5,46 +5,38 @@ namespace ZombieGame
     public class World
     {
         private IGameObject[] agents;
-        private IGameObject[,] grid;
-        public int Row { get; }
-        public int Column { get; }
+        public IGameObject[,] Grid { get; set; }
+        public int Row { get; set; }
+        public int Column { get; set; }
         Random rnd = new Random();
 
         public World(Config c)
         {
             agents = new IGameObject[c.InitialHumans + c.InitialZombies];
 
-            grid = new IGameObject[Row, Column];
+            Grid = new IGameObject[c.Row, c.Column];
 
-            for (int i = 0; i < grid.GetLength(0); i++)
+            for (int i = 0; i < Grid.GetLength(0); i++)
             {
-                for (int j = 0; j < grid.GetLength(1); j++)
+                for (int j = 0; j < Grid.GetLength(1); j++)
                 {
-                    grid[i, j] = new Empty();
+                    Grid[i, j] = new Empty();
                 }
             }
 
-            UserInterface it = new UserInterface();
-            it.ShowWorld(grid);
-
             // This for runs through the array of agents
-            for (int i = 0; i < agents.Length; i++)
+            for (int i = 0; i < c.InitialHumans; i++)
             {
                 Agent a = new Agent(AgentType.Human);
                 agents[i] = a;
-                Row = Convert.ToInt32(rnd);
-                Column = Convert.ToInt32(rnd);
-                grid[Row, Column] = a;
             }
-            for (int i = 0; i < agents.Length; i++)
+            for (int i = 0; i < c.InitialZombies; i++)
             {
                 Agent a = new Agent(AgentType.Zombie);
-                agents[i] = a;
-                Row = Convert.ToInt32(rnd);
-                Column = Convert.ToInt32(rnd);
-                grid[Row, Column] = a;
+                agents[i + c.InitialHumans] = a;
             }
 
+            Spawn(c);
         }
         // Fisher Yates method
         public void Shuffle()
@@ -54,6 +46,17 @@ namespace ZombieGame
                 Agent temp = (Agent)agents[i];
                 agents[i] = agents[j];
                 agents[j] = temp;
+            }
+        }
+
+        public void Spawn(Config c) {
+            for (int i = 0; i < agents.Length; i++) {
+                do {
+                    Row = rnd.Next(c.Row -1);
+                    Column = rnd.Next(c.Column -1);
+                } while (Grid[Row, Column] is Agent);
+
+                    Grid[Row, Column] = agents[i];
             }
         }
     }
