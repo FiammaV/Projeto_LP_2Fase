ea9d0bddd76138be08dbf45df9a6327da2c1f8cd
FiammaV <ines.perdigao.nunes@gmail.com>
2018-06-30 17:57:19 +0100

    Adicionei comentários e o método ToString

--- ZombieGame/Agent.cs
+++ ZombieGame/Agent.cs
@@ -5,20 +5,43 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ZombieGame {
-    /// <summary>
-    /// Agent implement IGameObject
-    /// </summary>
     public class Agent : IGameObject {
-       // Properties
+       /// <summary>
+       ///  Column of the grid
+       /// </summary>
         public int Column { get; }
+        /// <summary>
+        /// Row of the grid
+        /// </summary>
         public int Row { get; }
+        /// <summary>
+        /// Enumeration of agents types
+        /// </summary>
         public AgentType Type { get; set; }
+        /// <summary>
+        /// A boolean to check if it's playable or not
+        /// </summary>
         public bool Playable { get; set; }
 
-        // Empty constructor 
+        /// <summary>
+        /// Initializes a new instance of the AgentType class
+        /// </summary>
+        /// <param name="agent"></param>
         public Agent(AgentType agent)
         {
             Type = agent;
+        }
+
+        public override string ToString() {
+            string type = "";
+
+            if(Type == AgentType.Human) {
+                type = "Human";
+            } else if (Type == AgentType.Zombie) {
+                type = "Zombie";
+            }
+
+            return type;
         }
     }
 }
