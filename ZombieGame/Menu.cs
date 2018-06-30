d6c7b73308a1a48478f1badd98cdf7322b0e29b0
sera pinta <serapinta90@hotmail.com>
2018-06-30 18:46:21 +0100

    Adicionado o método Rules

--- ZombieGame/Menu.cs
+++ ZombieGame/Menu.cs
@@ -16,28 +16,37 @@ namespace ZombieGame
 
         private Config c;
 
-        // Contructor iniciates the game (nao devolve nada MAS pode receber se tiveres cenas entre parentises)
+        /// <summary>
+        /// Contructor iniciates the game (nao devolve nada MAS pode receber se tiveres cenas entre parentises)
+        /// </summary>
+        /// <param name="c"></param>
         public Menu(Config c)
         {
             gameManager = new GameManager(c);
         }
-
+        /// <summary>
+        /// Draw's info of DrawMenu
+        /// </summary>
         public void DrawMenu()
         {
             // Clear console
             Console.Clear();
-            Console.WriteLine(" ╓━━━━━━━━━━━━━━━━━━━━━━━━━━╖");
-            Console.WriteLine(" ║     ZombieApocalypse     ║");
-            Console.WriteLine(" ║˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭║");
-            Console.WriteLine(" ║      1  Game     ♚       ║");
-            Console.WriteLine(" ║      2  Credits  ☰       ║");
-            Console.WriteLine(" ║      3  Exit     ✈       ║");
-            Console.WriteLine(" ╙˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭˭╜");
+            Console.WriteLine(" ╓━━━━━━━━━━━━━━━━━━━━━━━━╖");
+            Console.WriteLine(" ║    ZombieApocalypse    ║");
+            Console.WriteLine(" ║━━━━━━━━━━━━━━━━━━━━━━━━║");
+            Console.WriteLine(" ║       1  Game          ║");
+            Console.WriteLine(" ║       2  Rules         ║");
+            Console.WriteLine(" ║       3  Credits       ║");
+            Console.WriteLine(" ║       4  Exit          ║");
+            Console.WriteLine(" ╙━━━━━━━━━━━━━━━━━━━━━━━━╜");
 
             // Return the string of method "Options"
             Options(Console.ReadLine());
         }
-
+        /// <summary>
+        /// Read input of player
+        /// </summary>
+        /// <param name="option"></param>
         private void Options(string option)
         {
             switch (option)
@@ -46,17 +55,43 @@ namespace ZombieGame
                     Console.Clear();
                     gameManager.GameLoop();
                     break;
-
                 case "2":
-                    Credits();
+                    Rules();
                     break;
 
                 case "3":
+                    Credits();
+                    break;
+
+                case "4":
                     Environment.Exit(0);
                     break;
             }
+            DrawMenu();
         }
+        /// <summary>
+        /// Draw's info of Rules
+        /// </summary>
+        public void Rules()
+        {
 
+            Console.Clear();
+            Console.WriteLine(" ╓━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╖");
+            Console.WriteLine(" ║                    Rules                                           ║");
+            Console.WriteLine(" ║━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━║");
+            Console.WriteLine(" ║   Agent    ║   Color   ║   Current Player   ║   Controled Player   ║");
+            Console.WriteLine(" ║━━━━━━━━━━━━║━━━━━━━━━━━║━━━━━━━━━━━━━━━━━━━━║━━━━━━━━━━━━━━━━━━━━━━║");
+            Console.WriteLine(" ║   Human    ║   Green   ║                    ║                      ║");
+            Console.WriteLine(" ║━━━━━━━━━━━━║━━━━━━━━━━━║       Yellow       ║━━━━━━━━━━━━━━━━━━━━━━║");
+            Console.WriteLine(" ║   Zombie   ║    Red    ║                    ║                      ║");
+            Console.WriteLine(" ╙━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╜");
+            Console.WriteLine("  ");
+            Console.ReadKey();
+
+        }
+        /// <summary>
+        /// Draw's info of Credits
+        /// </summary>
         public void Credits()
         {
             Console.Clear();
