2c5b699f59e599ae72f501e7f21a751be6f6f3f8
sera pinta <serapinta90@hotmail.com>
2018-07-01 13:37:05 +0100

    Adicionado index a ao metodo ToString e sua variável

--- ZombieGame/Agent.cs
+++ ZombieGame/Agent.cs
@@ -22,26 +22,32 @@ namespace ZombieGame {
         /// A boolean to check if it's playable or not
         /// </summary>
         public bool Playable { get; set; }
-
+        /// <summary>
+        /// Index to indentify agents
+        /// </summary>
+        public int Index { get; set; }
         /// <summary>
         /// Initializes a new instance of the AgentType class
         /// </summary>
         /// <param name="agent"></param>
-        public Agent(AgentType agent)
+        public Agent(AgentType agent, int index)
         {
             Type = agent;
+            Index = index;
         }
 
         public override string ToString() {
             string type = "";
 
             if(Type == AgentType.Human) {
-                type = "Human '\u2663'";
+                type = string.Format("Human (\u2663{0:d2})", Index);
             } else if (Type == AgentType.Zombie) {
-                type = "Zombie '\u25B2'";
+                type = string.Format("Zombie (\u25B2{0:d2})", Index);
             }
 
             return type;
         }
+
+        
     }
 }
